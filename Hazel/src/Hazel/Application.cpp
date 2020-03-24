7c3544f2ebeb1ed2c888fb6e85242c4030df2cbd
blitzdawolf <botjes2@hotmail.com>
2020-03-24 23:45:03 +0100

    Ok then

---
+++ Hazel/src/Hazel/Application.cpp
@@ -0,0 +1,28 @@
+#include "Application.h"
+
+#include "Log.h"
+#include "Events/ApplicationEvent.h"
+
+namespace Hazel {
+	Application::Application() {
+
+	}
+
+	Application::~Application() {
+
+	}
+
+	void Application::Run() {
+		WindowResizeEvent e(1280, 720);
+		if (e.IsInCategory(EventCategoryApplication))
+		{
+			HZ_TRACE(e);
+		}
+		if (e.IsInCategory(EventCategoryInput))
+		{
+			HZ_TRACE(e);
+		}
+
+		while (true) {}
+	}
+}
---
+++ Hazel/src/Hazel/Core.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifdef HZ_PLATFORM_WINDOWS
+	#ifdef HZ_BUILD_DLL
+		#define HAZEL_API __declspec(dllexport)
+	#else
+		#define HAZEL_API __declspec(dllimport)
+	#endif // HZ_BUILD_DLL
+#else
+
+#endif // HZ_PLTFORM_WINDOWS
+
+#define BIT(x) (1 << x)
