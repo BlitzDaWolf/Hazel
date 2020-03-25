54021d66bfa2df6dd13103ebc79c7f3183313ebf
blitzdawolf <botjes2@hotmail.com>
2020-03-25 01:18:45 +0100

    Yeet

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,29 +1,46 @@
 #include "hzpch.h"
 #include "Application.h"
 
-#include "Log.h"
-#include "Events/ApplicationEvent.h"
+#include "Hazel/Log.h"
+
+#include <GLFW/glfw3.h>
 
 namespace Hazel {
-	Application::Application() {
 
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
+	Application::Application()
+	{
+		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
-	Application::~Application() {
-
+	Application::~Application()
+	{
 	}
 
-	void Application::Run() {
-		WindowResizeEvent e(1280, 720);
-		if (e.IsInCategory(EventCategoryApplication))
-		{
-			HZ_TRACE(e);
-		}
-		if (e.IsInCategory(EventCategoryInput))
-		{
-			HZ_TRACE(e);
-		}
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
 
-		while (true) {}
+		HZ_CORE_TRACE("{0}", e);
+	}
+
+	void Application::Run()
+	{
+		while (m_Running)
+		{
+			glClearColor(1, 0, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
+			m_Window->OnUpdate();
+		}
+	}
+
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 }
